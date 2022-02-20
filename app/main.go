237c0cfdbba763677f6371dfb1858d5d476691c0
Ryan Adi <ryan.adi911@gmail.com>
2022-02-20 15:15:37 +0700

    add asset

--- app/main.go
+++ app/main.go
@@ -6,9 +6,11 @@ import (
 	_route "sirclo/project/capstone/delivery/routers"
 	"sirclo/project/capstone/util"
 
+	_assetController "sirclo/project/capstone/delivery/controllers/asset"
 	_authController "sirclo/project/capstone/delivery/controllers/auth"
 	_userController "sirclo/project/capstone/delivery/controllers/user"
 
+	_assetRepo "sirclo/project/capstone/repository/asset"
 	_authRepo "sirclo/project/capstone/repository/auth"
 	_userRepo "sirclo/project/capstone/repository/user"
 
@@ -29,18 +31,20 @@ func main() {
 	// initialize model
 	authRepo := _authRepo.NewAuthRepo(db)
 	userRepo := _userRepo.NewUserRepo(db)
+	assetRepo := _assetRepo.NewAssetRepo(db)
 
 	// initialize controller
 	authController := _authController.NewAuthController(authRepo)
 	userController := _userController.NewUserController(userRepo)
+	assetController := _assetController.NewAssetController(assetRepo)
 
 	// create new echo
 	e := echo.New()
 
 	e.Pre(middleware.RemoveTrailingSlash(), middleware.CORS())
 
-	_route.RegisterPath(e, authController, userController)
+	_route.RegisterPath(e, authController, userController, assetController)
 
 	// start the server, and log if it fails
-	e.Logger.Fatal(e.Start(":8080"))
+	e.Logger.Fatal(e.Start(":80"))
 }
