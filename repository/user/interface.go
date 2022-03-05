15c823a00d94775e6368e66d4fce0f95f56624a4
Ryan Adi <ryan.adi911@gmail.com>
2022-03-05 12:42:46 +0700

    add user_test.go

--- repository/user/interface.go
+++ repository/user/interface.go
@@ -8,7 +8,7 @@ type UserRepo interface {
 	Create(entities.User) error
 	Get() ([]entities.User, error)
 	GetById(int) (entities.User, error)
-	Update(entities.User, int) error
-	Delete(int) error
 	GetEmail() ([]entities.User, error)
+	// Update(entities.User, int) error
+	// Delete(int) error
 }
